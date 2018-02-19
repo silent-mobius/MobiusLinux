caa8f64bebe7ccf099ffa250cd03722e80787a1f
silent-mobius <alex@vaiolabs.com>
2018-02-19 13:32:53 +0200

    changing install packages chroot_head.sh

--- default/custom_setup/default.sh
+++ default/custom_setup/default.sh
@@ -1,3 +1,4 @@
-apt-get install -y nano iftop htop openssh-client openssh-server less openssl ca-certificates
+apt-get install -y nano vim htop openssh-client openssh-server less openssl ca-certificates
+
 echo -e "\033[31mPassword for root\033[0m"
 passwd root
